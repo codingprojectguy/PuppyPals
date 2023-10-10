66905917204c6f41d781793d7232ca662404bc07
Home PC <vfxprojectguy@gmail.com>
2023-10-10 13:27:49 -0700

    select a featured puppy

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,40 @@ import { puppyList } from "./data";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    // some logic here
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <div className="App">
+      {" "}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
     </div>
   );
 }
-
 export default App;
 
 // {puppies.map((puppy) => {
@@ -24,6 +48,6 @@ export default App;
 //       <h3>{email}</h3>
 //       <h3>{age}</h3>
 //       <h3>{ownerId}</h3>
-//     </div>
+//     </div>nn
 //   );
 // })}
