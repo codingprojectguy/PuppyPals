601b3270021786bdbf72891ad8d898ac823e75b5
Home PC <vfxprojectguy@gmail.com>
2023-10-10 12:42:27 -0700

    add data.js and app puppy func

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import { useState } from "react";
+import { puppyList } from "./data";
 
 function App() {
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
+
+// {puppies.map((puppy) => {
+//   const [id, name, email, age, ownerId] = puppy;
+//   return (
+//     <div key={id}>
+//       <h2>{name}</h2>
+//       <h3>{email}</h3>
+//       <h3>{age}</h3>
+//       <h3>{ownerId}</h3>
+//     </div>
+//   );
+// })}
